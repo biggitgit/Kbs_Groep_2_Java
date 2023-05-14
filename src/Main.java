6776b9a20efd1d5eadd937eb7656cc9033069fa6
Mounir Komenan <s1188188@student.windesheim.nl>
2023-05-14 16:03:22 +0200

    Inlogpagina en Steden kiezenpagina.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.sql.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hallo");
-    }
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        String url = "jdbc:mysql://localhost/NerdyGadgets";
+
+        try (Connection conn = DriverManager.getConnection(url,"root", "")) {
+            if (conn != null) {
+                System.out.println("Verbinding gelukt");
+            } else {
+                System.out.println("fout1");
+            }
+        }   catch (SQLException e){
+            System.err.println(e.getMessage() + "fout 2");
+        }
+
+        rbAP rbAP1 = new rbAP();
+}
 }
---
+++ src/rbAP.java
@@ -0,0 +1,118 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class rbAP extends JFrame implements ActionListener {
+
+    JFrame loginP = new JFrame("Inloggen");
+    JFrame tF = new JFrame("Steden kiezen");
+    JFrame RB = new JFrame("Routebepaling");
+    String wachtwoord1 = "hallo";
+    JLabel l1,l2;
+    JPasswordField p1;
+    JButton b1,b2;
+    JCheckBox s1,s2,s3,s4;
+    JCheckBox[] stedenCB;
+    ArrayList<Integer> gekozenSteden = new ArrayList<>();
+    public rbAP(){
+        loginP.setSize(700, 500);
+        loginP.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginP.setLayout(null);
+
+
+
+        l1 = new JLabel("Vul het wachtwoord in:");
+        l1.setBounds(275,100,150,50);
+
+        p1 = new JPasswordField();
+        p1.setBounds(250,150,180,50);
+
+        b1 = new JButton("OK");
+        b1.setBounds(300,210,75,25);
+
+
+        loginP.add(l1);
+
+        loginP.add(p1);
+
+        loginP.add(b1);
+
+        b1.addActionListener(this);
+        p1.addActionListener(this);
+
+        loginP.setResizable(false);
+        loginP.setVisible(true);
+    }
+
+    public void kiesFrame(){
+        tF.setSize(700,500);
+        tF.setLayout(null);
+
+        l2 = new JLabel("Kies de steden voor uw route:");
+        l2.setBounds(20,20,200,20);
+
+        b2 = new JButton("Route bepalen");
+        b2.setBounds(520,20,150,75);
+        b2.addActionListener(this);
+
+        s1 = new JCheckBox("Almere");
+        s1.setBounds(20,50,100,20);
+        s2 = new JCheckBox("Amsterdam");
+        s2.setBounds(20,80,100,20);
+        s3 = new JCheckBox("Den haag");
+        s3.setBounds(20,110,100,20);
+        s4 = new JCheckBox("Rotterdam");
+        s4.setBounds(20,140,100,20);
+
+        stedenCB = new JCheckBox[]{s1, s2, s3, s4};
+
+        tF.add(b2);
+        tF.add(l2);
+        tF.add(s1);
+        tF.add(s2);
+        tF.add(s3);
+        tF.add(s4);
+
+        tF.setResizable(false);
+        tF.setVisible(true);
+    }
+
+    public void RBframe(){
+        RB.setSize(700,500);
+        RB.setLayout(null);
+
+
+
+        RB.setResizable(false);
+        RB.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1){
+            String ingevuldWW = new String(p1.getPassword());
+
+            if (ingevuldWW.equals(wachtwoord1)){
+                loginP.dispose();
+                kiesFrame();
+            } else {
+                b1.setBackground(Color.red);
+            }
+        }
+
+        if (e.getSource() == b2){
+            int i = 0;
+            for (JCheckBox stad: stedenCB) {
+                if (stad.isSelected()){
+                    gekozenSteden.add(i+1);
+                }
+                i++;
+            }
+            tF.dispose();
+            RBframe();
+        }
+
+    }
+}
